66d90e15ba5bd0c205c23ca6620eb2755252598f
MihirMoghariya <patelprahlad5588@gmail.com>
2024-12-18 13:56:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-echo 'hello world'
+<?php
+// backend/index.php
+$servername = "mysql";
+$username = "root";
+$password = "password";
+$dbname = "customer_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT message FROM greetings LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of the first row
+    $row = $result->fetch_assoc();
+    echo $row["message"];
+} else {
+    echo "No greetings found.";
+}
+
+$conn->close();
+?>
